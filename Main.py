7b93166e5313f85a909450700a22e3ad8c733482
kolejarz2009 <kolejarz2009@users.noreply.github.com>
2017-11-18 17:49:31 +0000

    v4 - Nothing works, still

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os
 import requests
@@ -11,7 +10,7 @@ if os.path.exists(InstallFile)==False: #Check If given file exists
 	print("File does not exist")
 	quit()
 
-def r_i_ReadScript(r_i_Name): #This works - Need to add decoder from Ansi!!
+def r_i_ReadScript(r_i_Name): #This works - Need to add decoder from Ansi!! #Works-NonAsni
 	f = open(r_i_Name, "r")
 	#f=g.decode('utf-8')
 	lines = f.readlines()	
@@ -24,32 +23,28 @@ def r_i_ReadScript(r_i_Name): #This works - Need to add decoder from Ansi!!
 		URL=DnList[0]
 		SaveFileAs_PreMod=DnList[1].split("\n")	#Get rid of \n at the end of the line
 		SaveFileAs=SaveFileAs_PreMod[0]	
+		return URL, SaveFileAs 
 		print("URL:", URL, "will be saved as:", SaveFileAs)
-		return URL, SaveFileAs #Store these variables for further use
 		print(lines[2])	
 		DescriptionLine=(lines[2].split("="))
-		print(DescriptionLine[0].decode(utf-8))##Check
+		#print(DescriptionLine[0].decode(utf-8))##Check
+		print("Downloading file(s)...")		
+		os.chdir(MaSzynaFolder)
+		os.chdir("download") 
+		r = requests.get(URL, stream=True)
+		with open(SaveFileTo, 'wb') as fd:
+			for chunk in r.iter_content(chunk_size=128):
+				fd.write(chunk)
 	else:
 		print("Looks like this isn't a suitable install file. Please try doing it manually.")
 
-def AskToContinue(): #Ask for permission to continue	
+def AskToContinue(): #Ask for permission to continue	#Works
 	AskUser=input("Continue with the installation process? [y/N]")
 	if AskUser=="Y" or AskUser=="y":
 		print("Downloading files, please wait.")
 	else:
 		print("Installation aborted! Only y or Y continues the installation")
 		quit()
-
-def cdEU07Download():
-	os.chdir(MaSzynaFolder)
-	os.chdir("download")
-
-def Download(URL, SaveFileTo):
-	cdEU07Download() 
-	r = requests.get(URL, stream=True)
-	with open(SaveFileTo, 'wb') as fd:
-		for chunk in r.iter_content(chunk_size=128):
-			fd.write(chunk)
 
 def ArchiveExtraction(ArchiveName, ContentsDestination=MaSzynaFolder):
 	ContentsDestination="/home/m/EU07Installer/pkg/"#Docelowo $MaSzynaFolder
@@ -78,9 +73,9 @@ def DetermineIfScriptOrArchive(InstallFile):
 	IsScript=True #Debug only	
 	if IsScript==True:	
 		r_i_ReadScript(InstallName)
-		AskToContinue()		
-		Download(URL, SaveFileAs)
+		AskToContinue()
 		ArchiveExtraction(SaveFileAs, ContentsDestination)	
 		if "[TEXTURES.TXT]\n" in lines:
 			TexturesTxtProcess()
 
+r_i_ReadScript(InstallFile)
